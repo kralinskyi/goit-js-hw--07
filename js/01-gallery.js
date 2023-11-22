95be4e2b80b555bb532e3f9961373f66ac43a0e7
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-11-22 15:21:14 +0200

    start task7-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function imagesMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+         <a class="gallery__link" href=${original}>
+           <img
+             class="gallery__image"
+             src=${preview}
+             data-source=${original}
+             alt=${description}
+           />
+         </a>
+       </li>`;
+    })
+    .join("");
+}
+
+gallery.insertAdjacentHTML("afterbegin", imagesMarkup(galleryItems));
+
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+  console.log(e);
+}
