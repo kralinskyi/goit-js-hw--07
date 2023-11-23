d9918554a041459eff3a40466de7f7e00d822bb5
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-11-23 16:44:43 +0200

    task 7-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = imagesMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+function imagesMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+         <a class="gallery__link" href=${original}>
+           <img
+             class="gallery__image"
+             src=${preview}
+             alt=${description}
+           />
+         </a>
+       </li>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
